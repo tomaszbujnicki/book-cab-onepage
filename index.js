bc9152140972999e447f2e2918b085eaaf8b3dc5
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2021-09-22 01:37:13 +0200

    add sections and nav

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import * as bootstrap from 'bootstrap';
+
+var myCollapse = document.querySelector('.navbar-collapse');
+var bsCollapse = new bootstrap.Collapse(myCollapse, {
+  toggle: false,
+});
+
+document.addEventListener('click', () => {
+  bsCollapse.hide();
+});
